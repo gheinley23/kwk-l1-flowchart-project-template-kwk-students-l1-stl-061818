c46f44f5870764d38dcc238b6c075f8e849cee0b
gheinley23 <gheinley23@jacksonindians.us>
2018-07-26 04:55:34 +0000

    automatically backed up by learn

---
+++ app/Travle_from_home.rb
@@ -0,0 +1,5 @@
+puts "What type of cuisine do you want to cook? Italian,British,Chinese,Indian,Mediterranean"
+
+puts "How much money are you willing to spend on ingredients? ($,$$,$$$)"
+
+puts "What level of culinary difficulty are you confortable with? (Easy,Medium,Hard)"
